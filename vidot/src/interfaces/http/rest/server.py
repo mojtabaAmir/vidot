438952d7487a87d81977f3ccafac545cf122b538
Mojtaba Amirabadian <mamirabadian@gmail.com>
2021-01-23 16:24:30 +0330

    slight change :

--- vidot/src/interfaces/http/rest/server.py
+++ vidot/src/interfaces/http/rest/server.py
@@ -1,12 +1,12 @@
 from flask import Flask, Blueprint
 
 class Server:
-    def __init__(self, config, user_router):
+    def __init__(self, config, user_controller):
         self.config = config
         self.app = Flask(__name__)
         
         # Register bp dynamically
-        self.app.register_blueprint(user_router.router, url_prefix='/{}/{}/user'.format(config["base_url"], config["app_version"]))
+        self.app.register_blueprint(user_controller.router, url_prefix='/{}/{}/user'.format(config["base_url"], config["app_version"]))
 
     def start(self):
         self.app.run(**self.config["app"])
